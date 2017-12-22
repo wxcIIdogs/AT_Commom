de7815f0642bf23ed331f51d94ddd4758d6b633e
wxc <853976858@qq.com>
2017-12-22 19:51:43 +0800

    wxc:init porject

---
+++ AT/AT.h
@@ -0,0 +1,16 @@
+#ifndef __AT_H__
+#define __AT_H__
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+
+#defien ARRAY(__X__)   (sizeof(__X__) / sizeof(__X__[0]))
+
+#define unsigned char  uchar
+#define unsigned int   uint
+
+
+
+
+#endif
